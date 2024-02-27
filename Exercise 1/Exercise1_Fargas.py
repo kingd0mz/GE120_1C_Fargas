484ed68a3852268177af48c91d327a3e89791ace
Dominic Jr Fargas <dcfargas@up.edu.ph>
2024-02-27 17:03:21 +0800

    Submit Exercise 1

--- Exercise 1/Exercise1_Fargas.py
+++ Exercise 1/Exercise1_Fargas.py
@@ -4,3 +4,29 @@ Dominic Fagas Jr.
 2015-42069
 """
 
+# dms = -0.00
+
+# dms = dms % 360
+# # dms = float(input("Enter a number: "))
+
+# # Kunin si degree part
+# degree = int(dms)
+
+# # Kunin si minutes
+# minutes = int((dms - degree) * 60)
+
+# seconds = round((dms - degree - minutes/60) * 3600,2)
+
+# print(f"DMS: {degree}-{minutes}-{seconds}")
+
+# MUST BE A STRING INPUT
+dms = "118-25-14.48"
+values = dms.split("-")
+
+degrees = int(values[0])
+minutes = int(values[1])
+seconds = float(values[2])
+
+dd = degrees + (minutes/60) + (seconds/3600)
+
+print("ETO YUNG VALUE:", round(dd,6))
